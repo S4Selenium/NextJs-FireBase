da0e9faa01760319889478e9dd1c5e896de49e7e
sarinps007 <sarinps007@gmail.com>
2024-05-06 04:03:00 +0300

    TaskOneInternship

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+"use client";
+import "./globals.css";
+import { auth } from "@/app/config";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <auth>{children}</auth>
+      </body>
+    </html>
+  );
+}
